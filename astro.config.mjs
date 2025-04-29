a6c4778f0991946e13c50396ebd18e243899162b
Bloosh <weesnowflakes@gmail.com>
2025-04-29 13:54:59 -0400

    change live site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blooshiez.github.io/tasohote-tsubomi/',
+	site: 'https://blooshiez.github.io/',
 	integrations: [
 		starlight({
 			title: 'Tasotsubo EN',
