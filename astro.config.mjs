34b0446e605ad516e6ccde80f11865d56b1a052a
Bloosh <weesnowflakes@gmail.com>
2025-04-29 14:01:11 -0400

    adjust base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blooshiez.github.io/',
-	base: '/tasohote-tsubomi/',
+	base: 'tasohote-tsubomi',
 	integrations: [
 		starlight({
 			title: 'Tasotsubo EN',
